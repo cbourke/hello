ca7be0960cd4c6d1eb1da0dac4145cf3faae63ec
Chris Bourke <cbourke@cse.unl.edu>
2020-06-12 18:18:12 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,14 @@
+/**
+ * A simple hello world program
+ *
+ * Author: Chris Bourke
+ * Date:   202x/xx/xx
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Hello World, I'm Chris Bourke!\n");
+    return 0;
+}
