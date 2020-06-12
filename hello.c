1dd51c8febd50111972bcc8979333442284e477d
Chris Bourke <cbourke@cse.unl.edu>
2020-06-12 20:11:16 +0000

    added my name and major

--- hello.c
+++ hello.c
@@ -12,5 +12,8 @@ int main(int argc, char **argv) {
     printf("Hello World, I'm Chris Bourke!!\n");
     printf("My major is Computer Science!\n");
 
+    printf("Hello World, I'm Lauren Bourke!!\n");
+    printf("My major is Art Education!\n");
+
     return 0;
 }
